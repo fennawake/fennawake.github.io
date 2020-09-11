62c99efa1fbb876efaa55f2add3f12b609d7db6b
Fernando Souza <fenna.wake@gmail.com>
2020-09-11 12:14:00 +1000

    First version

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// header sticky
+const header = document.querySelector("header")
+const logo = document.querySelector(".logo")
+
+window.onscroll = function() {
+    if(window.pageYOffset > 10) {
+        header.classList.add("nav-scrolled")
+        logo.src = "image/logo-fsouza-white-normal.svg"
+    }else {
+        header.classList.remove("nav-scrolled")
+        logo.src = "image/logo-fsouza-white-normal-1.svg"
+    }
+}
+
+
+
+
+
+// toggle menu
+let show = true
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {    
+    
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on")
+    show = !show
+})
+  
